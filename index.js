25e459f93fb2133252847465deb5607b61810a60
Chic0S87 <chicosepultura@gmail.com>
2023-09-10 17:45:55 -0300

    minimal change on routes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import routerPessoa from "./Router/RouterPessoas.js";
 import routerCidade from "./Router/RouterCidades.js";
+import rotaProduto from "./Router/rotaProduto.js";
+import rotaCategoriaProd from "./Router/rotaCategoriaProd.js";
 import cors from "cors";
 
 const porta=4016;
@@ -11,6 +13,8 @@ server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
 server.use("/pessoas", routerPessoa);
 server.use("/cidades", routerCidade);
+server.use("/produto", rotaProduto);
+server.use("/categoria", rotaCategoriaProd);
 server.listen(porta,hostname, () => {
   console.log("Backend ouvindo em http://"+hostname+":"+porta);
 });
