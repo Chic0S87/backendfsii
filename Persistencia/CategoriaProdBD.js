6052c99cbe86e19ae1f8364fcaaf1a6eb0bb228a
Chic0S87 <chicosepultura@gmail.com>
2023-10-02 19:50:32 -0300

    solving imports

--- Persistencia/CategoriaProdBD.js
+++ Persistencia/CategoriaProdBD.js
@@ -1,21 +1,21 @@
-import conectar from "./Conexao.js";
+import Conect from "./Conexao.js";
 import CategoriaProd from "../Modelo/CategoriaProd.js";
 
 export default class CategoriaProdutoBD {
   async incluir(categoriaProd) {
     if (categoriaProd instanceof CategoriaProd) {
-      const conexao = await conectar();
+      const conexao = await Conect();
       const sql = "INSERT INTO categoria_produto (categoria) VALUES (?)";
       const valores = [categoriaProd.categoria];
       const resultado = await conexao.query(sql, valores);
       conexao.release()
-      return resultado[0].insertId;
+      return await resultado[0].insertId;
     }
   }
 
   async alterar(categoriaProd) {
     if (categoriaProd instanceof CategoriaProd) {
-      const conexao = await conectar();
+      const conexao = await Conect();
       const sql = "UPDATE categoria_produto SET categoria = ? WHERE codigo = ?";
       const valores = [categoriaProd.categoria, categoriaProd.codigo];
       await conexao.query(sql, valores);
@@ -25,7 +25,7 @@ export default class CategoriaProdutoBD {
 
   async excluir(categoriaProd) {
     if (categoriaProd instanceof CategoriaProd) {
-      const conexao = await conectar();
+      const conexao = await Conect();
       const sql = "DELETE FROM categoria_produto WHERE codigo = ?";
       const valores = [categoriaProd.codigo];
       await conexao.query(sql, valores);
@@ -34,7 +34,7 @@ export default class CategoriaProdutoBD {
   }
 
   async consultar(termo) {
-    const conexao = await conectar();
+    const conexao = await Conect();
     const sql = "SELECT * FROM categoria_produto WHERE categoria LIKE ?";
     const valores = ["%" + termo + "%"];
     const [rows] = await conexao.query(sql, valores);
@@ -48,7 +48,7 @@ export default class CategoriaProdutoBD {
   }
 
   // async consultarCodigo(codigo) {
-  //   const conexao = await conectar();
+  //   const conexao = await Conect();
 
   //   const sql = "SELECT * FROM categoria_produto WHERE codigo = ?";
   //   const valores = [codigo];
