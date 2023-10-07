e8561f76dc4f51cadb17f8dc89d56df67daae8a3
Chic0S87 <chicosepultura@gmail.com>
2023-10-07 14:45:04 -0300

    Adicionando finally ao try

--- Modelo/Doacao.js
+++ Modelo/Doacao.js
@@ -1,75 +1,177 @@
-import DoacaoBD from "../Persistencia/DoacaoBD.js"
+import DoacaoBD from "../Persistencia/DoacaoBD.js";
 
-export default class Doacao{
-  #codigo
-  #data
-  #pessoa
-  #produto
+export default class Doacao {
+    #codigo;
+    #cpfPessoa;    
+    #dataDoacao;
+    #listaItens;
+    #quantidade;
 
-  constructor(codigo, data, pessoa, produto){
-    this.#codigo = codigo;
-    this.#data = data;
-    this.#pessoa = pessoa;
-    this.#produto = produto
-  }
-  
-  get codigo(){
-    return this.#codigo
-  }
-
-  set codigo(novoCodigo){
-    this.#codigo = novoCodigo
-  }
-
-  get data(){
-    return this.#data
-  }
-
-  set data(novaData){
-    this.#data = novaData
-  }
-
-  get pessoa(){
-    return this.#pessoa
-  }
-
-  set pessoa(novaPessoa){
-    this.#pessoa = novaPessoa
-  }
-
-  get produto(){
-    return this.#produto
-  }
-
-  set produto(novoProduto){
-    this.#produto = novoProduto
-  }
-
-  toJSON(){
-    return{
-    "codigo":this.#codigo,
-    "data":this.#data,
-    "pessoa":this.#pessoa,
-    "produto":this.#produto
+    constructor(codigo, cpfPessoa, dataDoacao, listaItens) {
+        this.#codigo = codigo;
+        this.#cpfPessoa = cpfPessoa;
+        this.#dataDoacao = dataDoacao;
+        this.#listaItens = listaItens;
     }
-  }
 
-  async gravar(){
-    const doacaoBD = new DoacaoBD();
-    await doacaoBD.gravar(this);
-  }
+    get codigo() {
+        return this.#codigo;
+    }
 
-  async consultar(){
-    const doacaoBD = new DoacaoBD();
-    const doacao = await doacaoBD.consultar(this);
-    return doacao;
-  }
+    set codigo(codigo) {
+        this.#codigo = codigo;
+    }
 
-  async consultarCodigo(codigo){
-    const doacaoBD = new DoacaoBD();
-    const doacao = await doacaoBD.consultarCodigo(codigo);
-    return doacao;
-    
-  }
+    get cpfPessoa() {
+        return this.#cpfPessoa;
+    }
 
+    set cpfPessoa(newcpfPessoa) {
+        this.#cpfPessoa = newcpfPessoa;
+    }
+
+    get quantidade() {
+        return this.#quantidade;
+    }
+
+    set quantidade(newQuantidade) {
+        this.#quantidade = newQuantidade;
+    }
+
+    get dataDoacao() {
+        return this.#dataDoacao;
+    }
+
+    set dataDoacao(newDataDoacao) {
+        this.#dataDoacao = newDataDoacao;
+    }
+
+    get listaItens() {
+        return this.#listaItens
+    }
+
+    set listaItens(newListaItens) {
+        this.#listaItens = newListaItens;
+    }
+
+    toJSON() {
+        return {
+            "codigo": this.#codigo,
+            "cpfPessoa": this.#cpfPessoa,
+            "dataDoacao": this.#dataDoacao,
+            "listaItens": this.#listaItens.map(item => ({
+                "produto": item.produto,
+                "quantidade": item.quantidade
+            }))
+        };
+    }
+
+    async gravar() {
+        const doacaoBD = new DoacaoBD();
+        await doacaoBD.gravar(this);
+    }
+
+    // async atualizar() {
+    //     const doacaoBD = new DoacaoBD();
+    //     await doacaoBD.alterar(this);
+    // }
+
+    // async remover(id) {
+    //     const doacaoBD = new DoacaoBD();
+    //     await doacaoBD.excluir(id);
+    // }
+
+    async consultar(termo) {
+        const doacaoBD = new DoacaoBD();
+        const doacoes = await doacaoBD.consultar(termo);
+        return doacoes;
+    }
+
+    async consultarCodigo(codigo){
+           const doacaoBD = new DoacaoBD();
+           const doacao = await doacaoBD.consultarCodigo(codigo);
+           return doacao;
+    }
 }
+
+
+
+
+
+
+
+// import DoacaoBD from "../Persistencia/DoacaoBD.js"
+
+// export default class Doacao{
+//   #codigo
+//   #data
+//   #pessoa
+//   #produto
+
+//   constructor(codigo, data, pessoa, produto){
+//     this.#codigo = codigo;
+//     this.#data = data;
+//     this.#pessoa = pessoa;
+//     this.#produto = produto
+//   }
+  
+//   get codigo(){
+//     return this.#codigo
+//   }
+
+//   set codigo(novoCodigo){
+//     this.#codigo = novoCodigo
+//   }
+
+//   get data(){
+//     return this.#data
+//   }
+
+//   set data(novaData){
+//     this.#data = novaData
+//   }
+
+//   get pessoa(){
+//     return this.#pessoa
+//   }
+
+//   set pessoa(novaPessoa){
+//     this.#pessoa = novaPessoa
+//   }
+
+//   get produto(){
+//     return this.#produto
+//   }
+
+//   set produto(novoProduto){
+//     this.#produto = novoProduto
+//   }
+
+//   toJSON(){
+//     return{
+//     "codigo":this.#codigo,
+//     "data":this.#data,
+//     "pessoa":this.#pessoa,
+//     "produto":this.#produto
+//     }
+//   }
+
+//   async gravar(){
+//     const doacaoBD = new DoacaoBD();
+//     await doacaoBD.gravar(this);
+//   }
+
+//   async consultar(){
+//     const doacaoBD = new DoacaoBD();
+//     const doacao = await doacaoBD.consultar(this);
+//     return doacao;
+//   }
+
+//   async consultarCodigo(codigo){
+//     const doacaoBD = new DoacaoBD();
+//     const doacao = await doacaoBD.consultarCodigo(codigo);
+//     return doacao;
+    
+//   }
+
+// }
--- Persistencia/DoacaoBD.js
+++ Persistencia/DoacaoBD.js
@@ -1,95 +1,221 @@
 import Doacao from "../Modelo/Doacao.js";
 import Conect from "./Conexao.js";
-import ItemDoado from "../Modelo/ProdutoDoado.js";
-import Produto from "../Modelo/ProdutoPid.js";
+import Produto from "../Modelo/Produto.js";
 import CategoriaProd from "../Modelo/CategoriaProd.js";
 import Pessoas from "../Modelo/Pessoas.js";
 
-export default class DoacaoBD{
-  async gravar(doacao){
-    if (doacao instanceof Doacao){
-      const conexao = await Conect();
-      try{      
-      await conexao.beginTransaction();
-      const sql = "INSERT INTO doacao(dataDoacao, cpfPessoa) VALUES(?,?)"
-      const valores = [doacao.data, doacao.pessoa.cpf];
-      const resultado = await conexao.query(sql, valores);
-      doacao.codigo = resultado[0].insertId;
-      for(const item of doacao.produto){
-        const sql2 = "INSERT INTO doacao_produto(codigoProduto, codigoDoacao) VALUES (?,?)"
-        const parametros = [item.codigoProduto, item.codigoDoacao];
-        await conexao.query(sql2, parametros);
-      }
-      }catch(e){
-        await conexao.rollback();
-        throw e;
-      }
-      await conexao.commit();
-      global.conexao.release()
-    }
-  }
+export default class DoacaoBD {
 
-  async consultar(){
-    let listaDoacoes = [];
-    const conexao = await Conect();
-    const sql = "SELECT * FROM doacao as d inner join pessoas as p \
-                                on p.cpf = d.cpfPessoa\
-                                order by d.dataDoacao"
-    const [doacoes] = await conexao.query(sql);
-    for(const rows of doacoes){
-        const pessoa = new Pessoas(rows["cpf"], rows["nome"], rows["nascimento"], rows["endereco"], rows["cidade"], rows["telefone"], rows["tipo"], rows["profissao1"], rows["email"]);
-        let doacao = new Doacao(rows["codigo"], rows["dataDoacao"], pessoa, rows["codigoProduto"],[]);
-    
-    const sqlitens = "SELECT * FROM produto as pr inner join doacao_produto as i\
-                                                  inner join categoria_produto as c\
-                                                  on pr.codigo = i.codigoProduto\
-                                                  and c.codigo = pr.codigo\
-                                                  WHERE i.codigoDoacao = ?"       
-    const parametros=[doacao.codigo];
-    const [itensDoacao] = await conexao.query(sqlitens, parametros);
-    let listaItens = [];  
-    for(const item of itensDoacao){
-      const categoria= new CategoriaProd(item["codigo"], item["categoria"]);
-      const produto = new Produto(item["codigo"], item["nome"], item["metrica"], item["descricao"], item["codigoCategoria"], item["categoria"],categoria);
-      listaItens.push(new ItemDoado(produto, item["produto"]))
-    }
-    doacao.listaItens = listaItens;
-    listaDoacoes.push(doacao);
-    }
-    return listaDoacoes;
-  }
+    async gravar(doacao) {
+        if (doacao instanceof Doacao) {
+            const conexao = await Conect();
+            try {
+                await conexao.beginTransaction();
+                const sql = "INSERT INTO doacao(dataDoacao, cpfPessoa) VALUES (?,?)";
+                const valores = [doacao.dataDoacao, doacao.cpfPessoa];
+                const resultado = await conexao.query(sql, valores);
+                doacao.codigo = resultado[0].insertId;
 
-  async consultarCodigo(codigo){
-    let listaDoacoes = [];
-    const conexao = await Conect();
-    const sql = "SELECT * FROM doacao as d inner join pessoas as p \
-                                on p.cpf = d.cpfPessoa\
-                                WHERE d.codigo = ?\
-                                order by d.dataDoacao"
-    const parametros = [codigo];
-    const [doacoes] = await conexao.query(sql, parametros);
-    for(const rows of doacoes){
-        const pessoa = new Pessoas(rows["cpf"], rows["nome"], rows["nascimento"], rows["endereco"], rows["cidade"], rows["telefone"], rows["tipo"], rows["profissao1"], rows["email"]);
-        const doacao = new Doacao(rows["codigo"], rows["dataDoacao"], pessoa, rows["codigoProduto"],[]);
+                for (const item of doacao.listaItens) {
+                    const sql2 = "INSERT INTO doacao_produto(codigoProduto, codigoDoacao, quantidade) VALUES (?,?,?)";
+                    const parametros = [item.codigoProduto, doacao.codigo, item.quantidade];
+                    await conexao.query(sql2, parametros);
+                }
+
+                await conexao.commit();
+            } catch (e) {
+                await conexao.rollback();
+                throw e;
+            } finally {
+                conexao.release();
+            }
+        }
+    }
+
+
+    async consultar() {
+        const listaDoacoes = [];
+        const conexao = await Conect();
+
+        try {
+            const sql = "SELECT * FROM doacao as d \
+                  INNER JOIN pessoas as p ON p.cpf = d.cpfPessoa \
+                  ORDER BY d.dataDoacao";
+
+            const [doacoes] = await conexao.query(sql);
+
+            for (const rows of doacoes) {
+                const pessoa = new Pessoas(rows["cpf"], rows["nome"], rows["nascimento"], rows["endereco"], rows["cidade"], rows["telefone"], rows["tipo"], rows["profissao1"], rows["email"]);
+                const doacao = new Doacao(rows["codigo"], rows["dataDoacao"], pessoa, []);
+
+                const sqlitens = "SELECT pr.*, dp.*, cp.codigo AS codigoCategoria, cp.categoria FROM doacao_produto as dp \
+                          INNER JOIN produto as pr ON dp.codigoProduto = pr.codigo \
+                          INNER JOIN categoriaproduto as cp ON pr.categoria = cp.codigo \
+                          WHERE dp.codigoDoacao = ?";
+
+                const parametros = [doacao.codigo];
+                const [itensDoacao] = await conexao.query(sqlitens, parametros);
+                const listaItens = [];
+
+                for (const item of itensDoacao) {
+                    const categoria = new CategoriaProd(item["codigoCategoria"], item["categoria"]);
+                    const produto = new Produto(item["codigo_produto"], item["nome"], item["metrica"], item["descricao"], categoria.codigo, categoria.categoria);
+
+                    listaItens.push({ produto, quantidade: item["quantidade"] });
+                }
+
+                doacao.listaItens = listaItens;
+                listaDoacoes.push(doacao);
+            }
+
+            return listaDoacoes;
+        } finally {
+            conexao.release();
+        }
+    }
+
+    async consultarCodigo(codigo) {
+        const listaDoacoes = [];
+        const conexao = await Conect();
+
+        try {
+            const sql = "SELECT * FROM doacao as d \
+                  INNER JOIN pessoas as p ON p.cpf = d.cpfPessoa \
+                  WHERE d.codigo = ? \
+                  ORDER BY d.dataDoacao";
+
+            const parametros = [codigo];
+            const [doacoes] = await conexao.query(sql, parametros);
+
+            for (const rows of doacoes) {
+                const pessoa = new Pessoas(rows["cpf"], rows["nome"], rows["nascimento"], rows["endereco"], rows["cidade"], rows["telefone"], rows["tipo"], rows["profissao1"], rows["email"]);
+                const doacao = new Doacao(rows["codigo"], rows["dataDoacao"], pessoa, []);
+
+                const sqlitens = "SELECT * FROM doacao_produto as dp \
+                          INNER JOIN produto as pr ON dp.codigoProduto = pr.codigo \
+                          INNER JOIN categoriaproduto as cp ON pr.categoria = cp.codigo \
+                          WHERE dp.codigoDoacao = ?";
+
+                const parametros = [doacao.codigo];
+                const [itensDoacao] = await conexao.query(sqlitens, parametros);
+                const listaItens = [];
+
+                for (const item of itensDoacao) {
+                    const categoria = new CategoriaProd(item["codigo_categoria"], item["categoria"]);
+                    const produto = new Produto(item["codigoProduto"], item["nome"], item["metrica"], item["descricao"], categoria);
+                    listaItens.push({ produto, quantidade: item["quantidade"] });
+                }
+
+                doacao.listaItens = listaItens;
+                listaDoacoes.push(doacao);
+            }
+
+            return listaDoacoes;
+        } finally {
+            conexao.release();
+        }
+    }
+
     
-    const sqlitens = "SELECT * FROM produto as pr inner join doacao_produto as i\
-                                                  inner join categoria_produto as c\
-                                                  on pr.codigo = i.codigoProduto\
-                                                  and c.codigo = pr.codigo\
-                                                  WHERE i.codigoDoacao = ?"       
-    const parametros=[doacao.codigo];
-    const [itensDoacao] = await conexao.query(sqlitens, parametros);
-    let listaItens = [];  
-    for(const item of itensDoacao){
-      const categoria= new CategoriaProd(item["codigo"], item["categoria"]);
-      const produto = new Produto(item["codigo"], item["nome"], item["metrica"], item["descricao"], item["codigoCategoria"], item["categoria"],categoria);
-      listaItens.push(new ItemDoado(produto, item["produto"]))
-    }
-    doacao.listaItens = listaItens;
-    listaDoacoes.push(doacao);
-    }
-    return listaDoacoes;
-  }
 }
 
-// 2:06:43
+
+
+
+
+
+// import Doacao from "../Modelo/Doacao.js";
+// import Conect from "./Conexao.js";
+// import ItemDoado from "../Modelo/ProdutoDoado.js";
+// import Produto from "../Modelo/ProdutoPid.js";
+// import CategoriaProd from "../Modelo/CategoriaProd.js";
+// import Pessoas from "../Modelo/Pessoas.js";
+
+// export default class DoacaoBD{
+//   async gravar(doacao){
+//     if (doacao instanceof Doacao){
+//       const conexao = await Conect();
+//       try{      
+//       await conexao.beginTransaction();
+//       const sql = "INSERT INTO doacao(dataDoacao, cpfPessoa) VALUES(?,?)"
+//       const valores = [doacao.data, doacao.pessoa.cpf];
+//       const resultado = await conexao.query(sql, valores);
+//       doacao.codigo = resultado[0].insertId;
+//       for(const item of doacao.produto){
+//         const sql2 = "INSERT INTO doacao_produto(codigoProduto, codigoDoacao) VALUES (?,?)"
+//         const parametros = [item.codigoProduto, item.codigoDoacao];
+//         await conexao.query(sql2, parametros);
+//       }
+//       }catch(e){
+//         await conexao.rollback();
+//         throw e;
+//       }
+//       await conexao.commit();
+//       global.conexao.release()
+//     }
+//   }
+
+//   async consultar(){
+//     let listaDoacoes = [];
+//     const conexao = await Conect();
+//     const sql = "SELECT * FROM doacao as d inner join pessoas as p \
+//                                 on p.cpf = d.cpfPessoa\
+//                                 order by d.dataDoacao"
+//     const [doacoes] = await conexao.query(sql);
+//     for(const rows of doacoes){
+//         const pessoa = new Pessoas(rows["cpf"], rows["nome"], rows["nascimento"], rows["endereco"], rows["cidade"], rows["telefone"], rows["tipo"], rows["profissao1"], rows["email"]);
+//         let doacao = new Doacao(rows["codigo"], rows["dataDoacao"], pessoa, rows["codigoProduto"],[]);
+    
+//     const sqlitens = "SELECT * FROM produto as pr inner join doacao_produto as i\
+//                                                   inner join categoria_produto as c\
+//                                                   on pr.codigo = i.codigoProduto\
+//                                                   and c.codigo = pr.codigo\
+//                                                   WHERE i.codigoDoacao = ?"       
+//     const parametros=[doacao.codigo];
+//     const [itensDoacao] = await conexao.query(sqlitens, parametros);
+//     let listaItens = [];  
+//     for(const item of itensDoacao){
+//       const categoria= new CategoriaProd(item["codigo"], item["categoria"]);
+//       const produto = new Produto(item["codigo"], item["nome"], item["metrica"], item["descricao"], item["codigoCategoria"], item["categoria"],categoria);
+//       listaItens.push(new ItemDoado(produto, item["produto"]))
+//     }
+//     doacao.listaItens = listaItens;
+//     listaDoacoes.push(doacao);
+//     }
+//     return listaDoacoes;
+//   }
+
+//   async consultarCodigo(codigo){
+//     let listaDoacoes = [];
+//     const conexao = await Conect();
+//     const sql = "SELECT * FROM doacao as d inner join pessoas as p \
+//                                 on p.cpf = d.cpfPessoa\
+//                                 WHERE d.codigo = ?\
+//                                 order by d.dataDoacao"
+//     const parametros = [codigo];
+//     const [doacoes] = await conexao.query(sql, parametros);
+//     for(const rows of doacoes){
+//         const pessoa = new Pessoas(rows["cpf"], rows["nome"], rows["nascimento"], rows["endereco"], rows["cidade"], rows["telefone"], rows["tipo"], rows["profissao1"], rows["email"]);
+//         const doacao = new Doacao(rows["codigo"], rows["dataDoacao"], pessoa, rows["codigoProduto"],[]);
+    
+//     const sqlitens = "SELECT * FROM produto as pr inner join doacao_produto as i\
+//                                                   inner join categoria_produto as c\
+//                                                   on pr.codigo = i.codigoProduto\
+//                                                   and c.codigo = pr.codigo\
+//                                                   WHERE i.codigoDoacao = ?"       
+//     const parametros=[doacao.codigo];
+//     const [itensDoacao] = await conexao.query(sqlitens, parametros);
+//     let listaItens = [];  
+//     for(const item of itensDoacao){
+//       const categoria= new CategoriaProd(item["codigo"], item["categoria"]);
+//       const produto = new Produto(item["codigo"], item["nome"], item["metrica"], item["descricao"], item["codigoCategoria"], item["categoria"],categoria);
+//       listaItens.push(new ItemDoado(produto, item["produto"]))
+//     }
+//     doacao.listaItens = listaItens;
+//     listaDoacoes.push(doacao);
+//     }
+//     return listaDoacoes;
+//   }
+// }
+
+// // 2:06:43
